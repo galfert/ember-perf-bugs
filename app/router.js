841734f4146c953f3a436e2748900836bfc9f821
Garret Alfert <alfert@wevelop.de>
2017-01-15 23:10:17 +0100

    Add articles
    Shows error when switching routes where only
    the param changes.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('articles', function() {
+    this.route('article', { path: ':article_id' });
+  });
 });
 
 export default Router;
---
+++ app/routes/articles/article.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model(params) {
+    return Ember.Object.create({id: params.article_id});
+  }
+
+});
+
